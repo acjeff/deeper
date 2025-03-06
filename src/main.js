f604cfb1bc2136a74db6870d475750dacf4d247e
acjeff <labaia33@gmail.com>
2025-03-06 07:27:15 +0000

    save/load but not loading in cells

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Phaser from "phaser";
-import PreloadScene from "./scenes/PreloadScene.js";
 import GameScene from "./scenes/GameScene.js";
+import MenuScene from "./scenes/MenuScene";
 
 window._renderDistance = 2;
 window._tileSize = 10;
@@ -20,14 +20,16 @@ window._randomElements = [
     {
         id: window._tileTypes.empty,
         count: 10000
-    }, {
-        id: window._tileTypes.water,
+    },
+    {
+        id: window._tileTypes.empty,
         count: 100000,
         widthRange: [20, 30],
         heightRange: [20, 30],
         edgeNoiseChance: 0.5,
         layerWeights: [10,0,0,0,0,0,0]
-    }, {
+    },
+    {
         id: window._tileTypes.stone,
         count: 100000,
         widthRange: [10, 150],
@@ -44,7 +46,7 @@ const config = {
         default: "arcade",
         arcade: {gravity: {y: 300}, debug: false}
     },
-    scene: [PreloadScene, GameScene],
+    scene: [MenuScene, GameScene],
 };
 
 const game = new Phaser.Game(config);
