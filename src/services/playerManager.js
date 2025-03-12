7ee3057edf5ec6fadcf829c6b2dd44b270b0b821
acjeff <labaia33@gmail.com>
2025-03-12 22:30:25 +0000

    health

--- src/services/playerManager.js
+++ src/services/playerManager.js
@@ -1,7 +1,7 @@
 export default class PlayerManager {
     constructor(scene) {
         this.scene = scene;
-        this.scene.startPoint = {x: 1610, y: 197};
+        this.scene.startPoint = {x: 1540, y: 197};
         let x = this.scene.playerX || this.scene.startPoint.x;
         let y = this.scene.playerY || this.scene.startPoint.y;
         this.scene.player = this.scene.physics.add.sprite(x, y, 'player');
@@ -18,7 +18,6 @@ export default class PlayerManager {
         this.scene.player.setDisplaySize(this.scene.playerSize, this.scene.playerSize);
         // this.scene.playerRect = this.scene.add.rectangle(x, y, this.scene.playerSize, this.scene.playerSize, 0xffb2fd);
         this.scene.playerLight = this.scene.lightingManager.addLight(this.scene.player.x, this.scene.player.y, this.scene.playerSize * 8, 1, window.lightColors[1], false);
-        this.scene.playerLight.off = true;
         this.scene.physics.add.collider(this.scene.player, this.scene.soilGroup, () => {
             const fallSpeed = this.scene.lastFallSpeed || 0; // use stored value
             const safeSpeed = 180; // speeds below this cause no damage
