f7d967d05e4d7a65c89506758b3fa55536cb1733
acjeff <labaia33@gmail.com>
2025-03-14 00:52:47 +0000

    env variables

--- src/firebaseconfig.js
+++ src/firebaseconfig.js
@@ -4,14 +4,14 @@ import { getFirestore, doc, setDoc, getDoc, getDocs, collection ,writeBatch} fro
 
 // Firebase configuration (replace with your actual Firebase config)
 const firebaseConfig = {
-    apiKey: "AIzaSyBSmLbAzrfqtd2k1gSl8dHNdnSiUpeOaYs",
-    authDomain: "deeper-fc745.firebaseapp.com",
-    databaseURL: "https://deeper-fc745-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "deeper-fc745",
-    storageBucket: "deeper-fc745.firebasestorage.app",
-    messagingSenderId: "132574676345",
-    appId: "1:132574676345:web:19433a404d5bff9c7cdba8",
-    measurementId: "G-5FGBQ4V8VX"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
