ebb8750ddb084b73d8a29226891df7644004e51d
acjeff <labaia33@gmail.com>
2025-03-14 10:08:23 +0000

    m

--- src/firebaseconfig.js
+++ src/firebaseconfig.js
@@ -2,16 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import { getFirestore, doc, setDoc, getDoc, getDocs, collection ,writeBatch} from "firebase/firestore";
 
-// Firebase configuration (replace with your actual Firebase config)
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID,
-    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyBSmLbAzrfqtd2k1gSl8dHNdnSiUpeOaYs",
+    authDomain: "deeper-fc745.firebaseapp.com",
+    databaseURL: "https://deeper-fc745-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "deeper-fc745",
+    storageBucket: "deeper-fc745.firebasestorage.app",
+    messagingSenderId: "132574676345",
+    appId: "1:132574676345:web:19433a404d5bff9c7cdba8",
+    measurementId: "G-5FGBQ4V8VX"
 };
 
 // Initialize Firebase
